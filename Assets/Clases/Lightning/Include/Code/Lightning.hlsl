9096bf906db787a02b4a97a60d1a0e52c859d486
José David Serna <83099671+JDSerna00@users.noreply.github.com>
2025-04-28 13:38:16 -0500

    LightingSessions

--- Assets/Clases/Lightning/Include/Code/Lightning.hlsl
+++ Assets/Clases/Lightning/Include/Code/Lightning.hlsl
@@ -1,4 +1,30 @@
-void GetMainLight_float(out float3 lightDirection)
+#ifndef LIGHTING_CUSTOM_INCLUDED
+
+#define LIGHTING_CUSTOM_INCLUDED
+
+//#include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Lighting.hlsl"
+void GetMainLight_float(float3 PositionWS, out half3 Color, out float3 Direction, out float ShadowAttenuation)
 {
-    lightDirection = normalize(float3(1, 1, -1));
+    #if defined(SHADERGRAPH_PREVIEW)
+    Color = 1,
+    Direction = normalize(float3(1, 1, -1));
+    ShadowAttenuation =1;
+    #else
+    float4 shadowCoord = TransformWorldToShadowCoord(PositionWS);
+    Light light = GetMainLight(shadowCoord);
+    Color = light.color;
+    Direction = light.direction;
+    ShadowSamplingData shadowSamplingData = GetMainLightShadowSamplingData();
+    float shadowIntensity = GetMainLightShadowStrength();
+    
+    ShadowAttenuation = SampleShadowmap(
+    shadowCoord, 
+    TEXTURE2D_ARGS(_MainLightShadowmapTexture, sampler_MainLightShadowmapTexture),
+    shadowSamplingData,
+    shadowIntensity,
+    false);
+    #endif
 }
+
+
+#endif
